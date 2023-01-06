05cd473a3259403d11c099d214d9dc167d6701aa
William Wittig <wittig.william12@yahoo.com>
2023-01-06 14:21:20 -0800

    Committing my initial project files

---
+++ SDEV372_VersionControl/src/main/java/com/example/sdev372_versioncontrol/Sdev372VersionControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.sdev372_versioncontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdev372VersionControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdev372VersionControlApplication.class, args);
+    }
+
+}
---
+++ SDEV372_VersionControl/src/test/java/com/example/sdev372_versioncontrol/Sdev372VersionControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sdev372_versioncontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdev372VersionControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
