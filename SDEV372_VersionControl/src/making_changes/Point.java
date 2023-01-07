b5bb0bff1155992baa87fdc9da636e6bc85e24ee
William Wittig <wittig.william12@yahoo.com>
2023-01-06 16:59:12 -0800

    Reverted to a previous version of my point class. Committing changes to alter the (x, y) coordinate to (x, y, z)

--- SDEV372_VersionControl/src/making_changes/Point.java
+++ SDEV372_VersionControl/src/making_changes/Point.java
@@ -1,10 +1,12 @@
 public class Point {
     private double x;
     private double y;
+    private double z;
 
-    public Point(double x, double y) {
+    public Point(double x, double y, double z) {
         this.x = x;
         this.y = y;
+        this.z = z;
     }
 
     public double getX() {
@@ -21,6 +23,26 @@ public class Point {
 
     public void setY(double y) {
         this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        Point point = (Point) object;
+        return java.lang.Double.compare(point.x, x) == 0 && java.lang.Double.compare(point.y, y) == 0 && java.lang.Double.compare(point.z, z) == 0;
+    }
+
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), x, y, z);
     }
 
     @java.lang.Override
