49316604ac747aeb27f9f955c284cd8ccc568f1c
William Wittig <wittig.william12@yahoo.com>
2023-01-06 16:38:10 -0800

    Added equals and hashcode methods to point class

--- SDEV372_VersionControl/src/making_changes/Point.java
+++ SDEV372_VersionControl/src/making_changes/Point.java
@@ -23,6 +23,18 @@ public class Point {
         this.y = y;
     }
 
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        Point point = (Point) object;
+        return java.lang.Double.compare(point.x, x) == 0 && java.lang.Double.compare(point.y, y) == 0;
+    }
+
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), x, y);
+    }
+
     @java.lang.Override
     public java.lang.String toString() {
         return "Point{" +
